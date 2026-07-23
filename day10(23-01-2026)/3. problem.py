cb6cdbe2f9b6a46dae9b4fc40abc4825209c6903
Aashritha <aashrithabadampudi@gmail.com>
2026-07-23 15:11:37 +0530

    Added

--- day10(23-01-2026)/3. problem.py
+++ day10(23-01-2026)/3. problem.py
@@ -1,15 +1,18 @@
 a="elephant"
 v="aeiouAEIOU"
+
 a=list(a)
-v=list(v)
-# print(a,"\n",v)
+
 l=0
 h=len(a)-1
+
 while l<h:
-    if l is v:
+    if a[l] not in v:
+        l+=1
+    elif a[h] not in v:
+        h-=1
+    else:
         a[l],a[h]=a[h],a[l]
         l+=1
-    if h is v:
-        a[l],a[h]=a[h],a[l]
         h-=1
 print("".join(a))

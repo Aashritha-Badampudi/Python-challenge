b61ca86f48db7ceac8554280e6c6ad16ad7d59ab
Aashritha <aashrithabadampudi@gmail.com>
2026-07-30 11:04:20 +0530

    Added

---
+++ day15/llprob.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,num):
+        self.data=num
+        self.add=None
+n1=Node(10)
+n2=Node(20)
+n3=Node(30)
+n4=Node(40)
+n1.add=n2
+n2.add=n3
+n3.add=n4
+head=n1
+tc=head
+while tc!=None:
+    print(tc.data)
+    tc=tc.add
+print(tc)

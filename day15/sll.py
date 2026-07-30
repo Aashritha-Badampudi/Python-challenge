4099c2b67146de9c446a71d51b92e1ba78d36da6
Aashritha <aashrithabadampudi@gmail.com>
2026-07-30 12:21:51 +0530

    Added

---
+++ day15/sll.py
@@ -0,0 +1,37 @@
+#Single linked list
+class Node:
+    def __init__(self,num):
+        self.data=num
+        self.next=None
+
+class sll:
+    def __init__(self):
+        self.head=None
+
+    def add_end(self,num):
+        new=Node(num)
+        if self.head is None:
+            self.head=new
+        else:
+            tc=self.head
+            while tc.next is not None:
+                tc=tc.next
+            tc.next=new
+
+    def psll(self):
+        if self.head is None:
+            print("Data is absent")
+        else:
+            tc=self.head
+            while tc is not None:
+                print(tc.data,end="->")
+                tc=tc.next
+            print(" TC jumped from train")
+
+t=sll()
+t.add_end(10)
+t.add_end(20)
+t.add_end(30)
+t.psll()
+t.add_end(40)
+t.psll()

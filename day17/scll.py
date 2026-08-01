3a2a46f665046346c26d8d726399947ebe79c6ec
Aashritha <aashrithabadampudi@gmail.com>
2026-08-01 14:16:51 +0530

    Added

---
+++ day17/scll.py
@@ -0,0 +1,52 @@
+'''
+    head
+     |
+     v
+
+      n1                 n2                 n3                 n4
++-------------+    +-------------+    +-------------+    +-------------+
+| data | next | -> | data | next | -> | data | next | -> | data | next |
+|  10  |  n2  |    |  20  |  n3  |    |  30  |  n4  |    |  40  | None |
++-------------+    +-------------+    +-------------+    +-------------+
+      ^                                                           |
+      |___________________________________________________________|
+
+Implementation of the node for scll      
+'''
+
+class Node:
+    def __init__(self,num):
+        self.data=num
+        self.next=None
+
+class scll:
+    def __init__(self):
+        self.head=None
+
+    def add_end(self,num):
+        new=Node(num)
+        if self.head==None:
+            self.head=new
+            self.head.next=new
+        else:
+            tc=self.head
+            while tc.next!=self.head:
+                tc=tc.next
+            tc.next=new
+            new.next=self.head
+
+    def pscll(self):
+        if self.head is None:
+            print("Empty")
+        else:
+            tc=self.head
+            while tc.next is not self.head:
+                print(tc.data,end="-->")
+                tc=tc.next
+            print(tc.data,"--> Connects to the head")
+
+t=scll()
+t.add_end(10)
+t.add_end(20)
+t.pscll()
+            

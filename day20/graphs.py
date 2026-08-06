c88202bf0ffa5384f2dd76504ee6c4dcf66f883a
Aashritha <aashrithabadampudi@gmail.com>
2026-08-06 10:39:56 +0530

    Added

--- day20/graphs.py
+++ day20/graphs.py
@@ -29,7 +29,13 @@ Graphs:
 
  => There are 2 ways to implement:
  1. Adjacency list **G = [(1,2),(2,3),(3,4),(4,1),(2,4),(1,3)]**
- 2. Adjacency matrix 
+ 2. Adjacency matrix :
+                       1 2 3 4 5
+                   1 [ 0 1 1 0 0 ]
+                   2 [ 1 0 1 1 0 ]
+                   3 [ 1 1 0 0 1 ]
+                   4 [ 0 1 0 0 1 ]
+                   5 [ 0 0 1 1 0 ]
 
 '''
 #Creating Adjacency list
